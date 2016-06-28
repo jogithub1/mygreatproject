9ef1973789a4dacfc16b697c4464e5afe08a5e31
jocelyn chene <jocelynchene@gmail.com>
2016-06-28 14:32:11 +0200

    pom + main

---
+++ src/main/java/my_great_project/Class2.java
@@ -0,0 +1,13 @@
+package my_great_project;
+
+import org.apache.commons.lang3.StringUtils;
+
+import com.google.common.base.Strings;
+
+
+public class Class2 {
+	public static void main (String[] args){
+	System.out.println(Strings.isNullOrEmpty("aaa"));
+	System.out.println(StringUtils.isEmpty("aaa")); 
+	}
+}
