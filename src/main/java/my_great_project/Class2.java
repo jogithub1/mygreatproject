ba67ec83f0fe13b3495972554cf9aa544db36401
jocelyn chene <jocelynchene@gmail.com>
2016-06-28 14:50:20 +0200

    Revert "blabla"
    This reverts commit 44208180e81019e28d4f197922d938017498f98a.

--- src/main/java/my_great_project/Class2.java
+++ src/main/java/my_great_project/Class2.java
@@ -9,6 +9,5 @@ public class Class2 {
 	public static void main (String[] args){
 	System.out.println(Strings.isNullOrEmpty("aaa"));
 	System.out.println(StringUtils.isEmpty("aaa")); 
-	//disc
 	}
 }
