271be6f2fd25a937aa0a13eeb22ca8e88bbe5a01
hela soussi <hela.soussii@gmail.com>
2016-06-28 16:43:54 +0200

    hela

--- src/main/java/my_great_project/Class2.java
+++ src/main/java/my_great_project/Class2.java
@@ -4,7 +4,7 @@ import org.apache.commons.lang3.StringUtils;
 
 import com.google.common.base.Strings;
 
-
+//hela
 public class Class2 {
 	public static void main (String[] args){
 	System.out.println(Strings.isNullOrEmpty("aaa"));
